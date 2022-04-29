6eb8f3c57a70aa21e3559164e16a35148104bd98
Kehayov98 <alexandar793@gmail.com>
2022-04-29 15:31:24 +0300

    Added whitenoise for static files

--- store/store/settings.py
+++ store/store/settings.py
@@ -49,6 +49,7 @@ STORE_APPS = [
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + STORE_APPS
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,12 +130,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'staticfiles',
+# ]
 
-STATICFILES_DIRS = [
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
     BASE_DIR / 'staticfiles',
-]
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
 MEDIA_URL = '/media/'
