d730c6acbcd454cdf01ce0d316f48c143dc3745e
Kehayov98 <alexandar793@gmail.com>
2022-04-29 18:43:17 +0300

    Fix settings

--- store/store/settings.py
+++ store/store/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS = [
@@ -145,7 +145,7 @@ STATICFILES_DIRS = [
 #     BASE_DIR / 'staticfiles',
 # )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
 MEDIA_URL = '/media/'
