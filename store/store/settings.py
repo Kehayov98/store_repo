3df7b9fa7be9ae87606561c9390f9a3ac346487c
Kehayov98 <alexandar793@gmail.com>
2022-04-29 16:31:43 +0300

    Add static

--- store/store/settings.py
+++ store/store/settings.py
@@ -130,20 +130,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-# STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'staticfiles',
-# ]
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
+STATICFILES_DIRS = [
     BASE_DIR / 'staticfiles',
-)
+]
+
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_URL = '/static/'
+#
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     BASE_DIR / 'staticfiles',
+# )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
